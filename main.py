30fd2156cab200462192024cbb7e54b3dc9b26c6
Jamiatul Khasanah <128022114+jamiatulkhasanah@users.noreply.github.com>
2023-03-21 08:29:38 +0000

    Update Biodata

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Jamiatul Khasanah</p>"
+  return "<p> Nama : Jamiatul Khasanah </p><p>Tempat,Tanggal Lahir : Malang, 11 Juni 2004</p><p> Alamat : Dusun.Boro Watugede, Singosari, Malang</p><p> Jenis Kelamin : Perempuan</p><p> Kewarganegaraan : Indonesia</p><p>Agama : Islam</p>"
 
 
 if __name__ == "__main__":
